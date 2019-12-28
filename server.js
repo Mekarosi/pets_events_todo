76644710ce5b461655b3d7507d451ffc3015eac3
Mekarosi Assa <mekarosi_assa@yahoo.com>
2019-12-28 18:30:02 +0100

    Create models

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 const express = require('express');
-const connectDB = require('./config/db');
-
 const app = express();
+const connectDB = require('./config/db');
+const cors = require('cors');
+const PORT = process.env.POST || 4000;
 
 //Connect Database
 connectDB();
 
 // Init Middleware
 app.use(express.json({ extended: false }));
-
-const PORT = process.env.POST || 4000;
-
+app.use(cors());
 app.get('/', (req, res) => res.send('API Running'));
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
