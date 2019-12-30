840998e8085e2cec77ee2ef5365a5d98a5f4311f
Mekarosi Assa <mekarosi_assa@yahoo.com>
2019-12-30 16:45:01 +0100

    Create Api to update todo

--- route/api/todos.js
+++ route/api/todos.js
@@ -39,20 +39,25 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-// router.get('/:id', async (req, res) => {
-//   try {
-//     let id = req.params.id;
+router.post('/update/:id', async (req, res) => {
+  try {
+    await Todo.findById(req.params.id, (err, todo) => {
+      if (!todo) res.status(404).send('data is not found');
+      else todo.todo_description = req.body.todo_description;
+      todo.todo_responsible = req.body.todo_responsible;
+      todo.todo_priority = req.body.todo_priority;
+      todo.todo_completed = req.body.todo_completed;
+      todo.todo_startdate = req.body.todo_startdate;
+      todo.todo_completeddate = req.body.todo_completeddate;
 
-//     Todo.findById(id, (err, todo) => {
+      todo.save().then(todo => {
+        res.json('Todo updated!');
+      });
+    });
+  } catch (err) {
+    console.error(err.message);
+    res.status(400).send('Update not possible');
+  }
+});
 
-//     if (id) {
-//       return res.json(todo);
-//     } else {
-//       res.status(400).send('id not found');
-//     }
-//   }
-//   } catch (err) {
-//     console.error(err.message);
-//   }
-// )}
 module.exports = router;
