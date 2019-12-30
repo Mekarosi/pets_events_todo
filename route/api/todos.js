904639f3e838f8bff7f8f6bb37ace40c2b24dde8
Mekarosi Assa <mekarosi_assa@yahoo.com>
2019-12-30 16:21:02 +0100

    Create Api to get todo by id

--- route/api/todos.js
+++ route/api/todos.js
@@ -25,4 +25,34 @@ router.post('/add', async (req, res) => {
   }
 });
 
+router.get('/:id', async (req, res) => {
+  try {
+    let id = req.params.id;
+    await Todo.findById(id, (err, todo) => {
+      res.json(todo);
+      if (err) {
+        return res.send('id does not exist');
+      }
+    });
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+// router.get('/:id', async (req, res) => {
+//   try {
+//     let id = req.params.id;
+
+//     Todo.findById(id, (err, todo) => {
+
+//     if (id) {
+//       return res.json(todo);
+//     } else {
+//       res.status(400).send('id not found');
+//     }
+//   }
+//   } catch (err) {
+//     console.error(err.message);
+//   }
+// )}
 module.exports = router;
