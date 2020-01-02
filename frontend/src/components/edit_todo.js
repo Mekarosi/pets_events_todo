74421e1eb640bcf9f4284e62b3dc5e1da2e8db12
Mekarosi Assa <mekarosi_assa@yahoo.com>
2020-01-02 11:54:57 +0100

    Add state properties to edit todos

--- frontend/src/components/edit_todo.js
+++ frontend/src/components/edit_todo.js
@@ -1,6 +1,20 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class Edit_todo extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      todo_description: '',
+      todo_responsible: '',
+      todo_priority: '',
+      todo_startdate: '',
+      todo_completeddate: '',
+      todo_completed: false
+    };
+  }
+
   render() {
     return (
       <div>
