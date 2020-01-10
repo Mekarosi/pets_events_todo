d862a253107e1927975136523ee1bf01d822f601
Mekarosi <mekarosi_assa@yahoo.com>
2020-01-10 15:12:20 +0100

    Prepare for deploy

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const app = express();
 const connectDB = require('./config/db');
 const cors = require('cors');
 const PORT = process.env.POST || 4000;
-require('dotenv').config();
+const path = require('path');
+// require('dotenv').config();
 
 //Connect Database
 connectDB();
@@ -14,5 +15,15 @@ app.use(cors());
 
 // Define Route
 app.use('/api/todos', require('./route/api/todos'));
+
+// Server static asset if in production
+if (process.env.NODE_ENV === 'production') {
+  //set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
