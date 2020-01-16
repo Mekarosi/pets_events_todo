7553d8a2c1b5d6b4d115404b34bdcf69ff3e7de9
Mekarosi <mekarosi_assa@yahoo.com>
2020-01-16 11:40:49 +0100

    make it better

---
+++ client/build/precache-manifest.db66a90e610fc86462ff40feaa7c50f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27bfb3637c7e56c360e279aae7e30eca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5dfe15b8b9150f669328",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "c41ac1ff512f05e5e5df",
+    "url": "/static/css/main.ca770969.chunk.css"
+  },
+  {
+    "revision": "5dfe15b8b9150f669328",
+    "url": "/static/js/2.7e9aa22d.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/static/js/2.7e9aa22d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c41ac1ff512f05e5e5df",
+    "url": "/static/js/main.a12b2861.chunk.js"
+  },
+  {
+    "revision": "05e2d2637e83c42dc136",
+    "url": "/static/js/runtime-main.4ee28378.js"
+  }
+]);
